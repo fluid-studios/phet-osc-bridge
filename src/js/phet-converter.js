59f8e53b66ce50a3cf21ea2a6ebd8d8f65763f77
Colin Clark <colinbdclark@gmail.com>
2017-06-12 18:25:33 -0400

    gh-3: Refactors conversion functions to avoid mutating shared data.

--- src/js/phet-converter.js
+++ src/js/phet-converter.js
@@ -25,114 +25,119 @@ fluid.defaults("phetosc.converter", {
     },
 
     invokers: {
-        toOSC: "phetosc.converter.toOSC({arguments}.0, {that}.options)"
+        toOSC: "phetosc.converter.toOSC({arguments}.0, {that})"
+    },
+
+    events: {
+        onError: null
+    },
+
+    listeners: {
+        "onError.logWarningMessage": {
+            funcName: "fluid.log",
+            args: [
+                fluid.logLevel.WARN,
+                "{arguments}.0", // Error message.
+                " Address: ",
+                "{arguments}.1",
+                " Value: ",
+                "{arguments}.2"
+            ]
+        }
     }
 });
 
-phetosc.converter.toOSC = function (phetEvent, options) {
-    var packets = [];
-    phetosc.converter.visitPhETEvents(phetEvent, options, packets,
-        phetosc.converter.eventToOSCMessage);
-
-    return packets;
+phetosc.converter.oscTypeForJSType = function (jsType, jsToOSCTypes) {
+    return jsToOSCTypes[jsType];
 };
 
-phetosc.converter.visitPhETEvents = function (phetEvent, options, packets, eventVisitor) {
-    eventVisitor(phetEvent, options, packets);
-
-    fluid.each(phetEvent.children, function (childEvent) {
-        phetosc.converter.visitPhETEvents(childEvent, options, packets, eventVisitor);
-    });
-};
-
-phetosc.converter.eventToOSCMessage = function (phetEvent, options, packets) {
-    var messageCollection,
-        togo;
-
-    if (options.bundleParameters) {
-        var bundle = phetosc.converter.createBundleForEvent(phetEvent, packets);
-        messageCollection = bundle.packets;
-        togo = bundle;
-    } else {
-        messageCollection = packets;
-        togo = messageCollection;
-    }
-
-    var addressPrefix = phetosc.converter.messageAddressPrefix(phetEvent, options);
-    phetosc.converter.objectToMessages(phetEvent.parameters,
-         addressPrefix, options, messageCollection);
-
-    return togo;
-};
-
-phetosc.converter.createBundleForEvent = function (phetEvent, packets) {
-    var bundle = {
-        packets: []
-    };
-
-    packets.push(bundle);
-
-    return bundle;
-};
-
-phetosc.converter.messageAddressPrefix = function (phetEvent, options) {
-    return fluid.stringTemplate(options.addressTemplate, phetEvent);
-};
-
-phetosc.converter.objectToMessages = function (obj, addressPrefix, options, packets) {
-    if (!obj || Object.keys(obj) < 1) {
-        // TODO: Rearrange argument order?
-        phetosc.converter.toMessage(undefined, addressPrefix, undefined, options, packets);
-        return;
-    }
-
-    addressPrefix = addressPrefix + "/";
-    fluid.each(obj, function (val, key) {
-        phetosc.converter.parameterToMessages(val, key, addressPrefix, options, packets);
-    });
-};
-
-phetosc.converter.parameterToMessages = function (val, key, addressPrefix, options, packets) {
-    if (fluid.isArrayable(val)) {
-        throw new Error("Array parameter value types are not yet supported. " +
-        "Can't convert parameter to an OSC messagee. Parameter value was: " +
-        fluid.prettyPrintJSON(val));
-    }
-
-    var address = addressPrefix + key,
-        parameterType = typeof val;
-
-    if (parameterType === "object") {
-        return phetosc.converter.objectToMessages(val, address, options, packets);
-    } else {
-        return phetosc.converter.toMessage(val, address, parameterType, options, packets);
-    }
-};
-
-phetosc.converter.toMessage = function (val, address, parameterType, options, packets) {
+phetosc.converter.toMessage = function (address, that, val, paramType) {
     var message = {
         address: address
     };
 
-    if (val !== undefined) {
-        message.args = [
-            {
-                type: phetosc.converter.oscTypeForJSType(parameterType, options.jsToOSCTypes),
-                value: val
-            }
-        ]
+    var togo = [message];
+
+    if (val === undefined) {
+        return togo;
     }
 
-    packets.push(message);
-};
-
-
-phetosc.converter.oscTypeForJSType = function (jsType, jsToOSCTypes) {
-    var oscType = jsToOSCTypes[jsType];
+    var oscType = phetosc.converter.oscTypeForJSType(paramType,
+        that.options.jsToOSCTypes);
 
     if (!oscType) {
-        throw new Error("An invalid JavaScript value type was found. Can't convert parameter to an OSC message. Parameter value was: " + fluid.prettyPrintJSON(val));
+        that.events.onError.fire("A parameter property was found with a type of " + paramType + ", which is not OSC compatible. It was omitted.", address, val);
+
+        return togo;
     }
 
-    return oscType;
+    message.args = [
+        {
+            type: oscType,
+            value: val
+        }
+    ];
+
+    return togo;
+};
+
+phetosc.converter.parameterToMessages = function (addressPrefix, that, val, key) {
+    if (fluid.isArrayable(val)) {
+        that.events.onError.fire("An Array-typed event parameter was found; conversion of Array parameters to OSC messages is not yet supported. It was omitted.", value, key);
+        return [];
+    }
+
+    var address = addressPrefix + "/" + key,
+        paramType = typeof val;
+
+    return paramType === "object" ?
+        phetosc.converter.objectToMessages(address, that, val) :
+        phetosc.converter.toMessage(address, that, val, paramType);
+};
+
+phetosc.converter.objectToMessages = function (addressPrefix, that, obj) {
+    if (!obj || Object.keys(obj) < 1) {
+        return phetosc.converter.toMessage(addressPrefix, that);
+    }
+
+    var messages = [];
+
+    fluid.each(obj, function (val, key) {
+        var paramMessages = phetosc.converter.parameterToMessages(addressPrefix, that, val, key);
+        messages.push(paramMessages);
+    });
+
+    return fluid.flatten(messages);
+};
+
+phetosc.converter.messageAddressPrefix = function (phetEvent, that) {
+    return fluid.stringTemplate(that.options.addressTemplate, phetEvent);
+};
+
+phetosc.converter.oscBundle = function (packets) {
+    return {
+        packets: packets
+    };
+};
+
+phetosc.converter.eventToOSCMessages = function (phetEvent, that) {
+    var addressPrefix = phetosc.converter.messageAddressPrefix(phetEvent, that);
+
+    var messages = phetosc.converter.objectToMessages(addressPrefix, that, phetEvent.parameters);
+
+    return !that.options.bundleParameters ?
+        messages : [phetosc.converter.oscBundle(messages)];
+};
+
+phetosc.converter.toOSC = function (phetEvent, that) {
+    // Convert the event body to a set of OSC messages.
+    var eventPackets = phetosc.converter.eventToOSCMessages(phetEvent, that);
+
+    // Then convert all its child events.
+    var childPackets = fluid.transform(phetEvent.children, function (child) {
+        return phetosc.converter.toOSC(child, that);
+    });
+
+    // Return back a consolidated array of bundles/messages.
+    return Array.prototype.concat.apply(eventPackets, childPackets);
 };
